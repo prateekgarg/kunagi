b9dd84a99a350f2585c6f8ac77ba348df1679c1d
Witoslaw Koczewski <wi@koczewski.de>
2010-11-08 21:13:38 +0100

    bugfix: selected entities

--- src/main/java/scrum/client/admin/ProjectUserConfig.java
+++ src/main/java/scrum/client/admin/ProjectUserConfig.java
@@ -1,10 +1,11 @@
 package scrum.client.admin;
 
+import ilarkesto.core.base.Utl;
 import ilarkesto.core.scope.Scope;
 import ilarkesto.gwt.client.DateAndTime;
 import ilarkesto.gwt.client.TimePeriod;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class ProjectUserConfig extends GProjectUserConfig {
@@ -38,18 +39,20 @@ public class ProjectUserConfig extends GProjectUserConfig {
 	}
 
 	public boolean addSelectedEntityId(String entityId) {
-		List<String> ids = getSelectedEntitysIds();
-		if (ids.contains(entityId)) return false;
-		ids.add(entityId);
-		setSelectedEntitysIds(ids);
+		// List<String> ids = getSelectedEntitysIds();
+		// if (ids.contains(entityId)) return false;
+		// ids.add(entityId);
+		// setSelectedEntitysIds(ids);
+		setSelectedEntitysIds(Utl.toList(entityId));
 		return true;
 	}
 
 	public boolean removeSelectedEntityId(String entityId) {
-		List<String> ids = getSelectedEntitysIds();
-		if (!ids.contains(entityId)) return false;
-		ids.remove(entityId);
-		setSelectedEntitysIds(ids);
+		// List<String> ids = getSelectedEntitysIds();
+		// if (!ids.contains(entityId)) return false;
+		// ids.remove(entityId);
+		// setSelectedEntitysIds(ids);
+		setSelectedEntitysIds(new ArrayList<String>(0));
 		return true;
 	}
 
