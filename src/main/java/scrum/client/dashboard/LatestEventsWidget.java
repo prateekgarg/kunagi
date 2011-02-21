91b311005f27dce1d94c5e74d0307538eec98820
Witoslaw Koczewski <wi@koczewski.de>
2011-02-22 00:09:00 +0200

    iss422 bugfix: user coloring on the dashboard (latest events)

--- src/main/java/scrum/client/dashboard/LatestEventsWidget.java
+++ src/main/java/scrum/client/dashboard/LatestEventsWidget.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -63,8 +63,9 @@ public class LatestEventsWidget extends AScrumWidget {
 	private String colorUsers(String html) {
 		Project project = getCurrentProject();
 		for (User user : project.getParticipants()) {
-			html = html.replace(user.getName(), "<span style='color: "
-					+ project.getUserConfig(user).getColor() + ";'>" + user.getName() + "</span>");
+			String key = user.getName() + " ";
+			html = html.replace(key,
+				"<span style='color: " + project.getUserConfig(user).getColor() + ";'>" + user.getName() + "</span> ");
 		}
 		return html;
 	}
