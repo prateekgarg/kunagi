b5e413cabca717e1cdeeddad6d11f6023e888972
Witoslaw Koczewski <wi@koczewski.de>
2011-03-17 01:05:00 +0200

    tsk566 bugfix: collapsing blocks

--- src/main/java/scrum/client/workspace/history/HistoryToken.java
+++ src/main/java/scrum/client/workspace/history/HistoryToken.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -55,6 +55,7 @@ public class HistoryToken {
 		String oldProjectId = projectId;
 		String oldPage = page;
 		String oldEntityId = entityId;
+		boolean oldToggle = toggle;
 
 		projectId = props.get("project");
 		page = props.get("page");
@@ -64,7 +65,7 @@ public class HistoryToken {
 		if (projectId == null || !Utl.equals(oldProjectId, projectId)) {
 			observer.onProjectChanged();
 			return;
-		} else if (!Utl.equals(oldPage, page) || !Utl.equals(oldEntityId, entityId)) {
+		} else if (!Utl.equals(oldPage, page) || !Utl.equals(oldEntityId, entityId) || toggle != oldToggle) {
 			observer.onPageOrEntityChanged();
 		} else {
 			log.debug("Nothing changed");
