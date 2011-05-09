7d0c454119568d4a7262a465d4102479050c178d
Witoslaw Koczewski <wi@koczewski.de>
2011-05-10 00:13:21 +0200

    base url for journal rss

--- src/main/java/scrum/server/journal/JournalRssServlet.java
+++ src/main/java/scrum/server/journal/JournalRssServlet.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -31,6 +31,7 @@ public class JournalRssServlet extends AHttpServlet {
 	private static final long serialVersionUID = 1;
 
 	private ProjectDao projectDao;
+	private ScrumWebApplication app;
 
 	@Override
 	protected void onRequest(HttpServletRequest req, HttpServletResponse resp, WebSession session) throws IOException {
@@ -39,14 +40,12 @@ public class JournalRssServlet extends AHttpServlet {
 		if (projectId == null) throw new RuntimeException("projectId == null");
 		Project project = projectDao.getById(projectId);
 		resp.setContentType("application/rss+xml");
-		String urlBase = req.getRequestURL().toString();
-		urlBase = urlBase.substring(0, urlBase.indexOf("/scrum.ScrumGwtApplication/") + 1);
-		project.writeJournalAsRss(resp.getOutputStream(), "UTF-8", urlBase);
+		project.writeJournalAsRss(resp.getOutputStream(), "UTF-8", app.getBaseUrl());
 	}
 
 	@Override
 	protected void onInit(ServletConfig config) {
-		ScrumWebApplication app = ScrumWebApplication.get(config);
+		app = ScrumWebApplication.get(config);
 		projectDao = app.getProjectDao();
 	}
 
