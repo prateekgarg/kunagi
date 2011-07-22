e57bdfcf78760a33cc3ea77d9a9d80e7ab5a7031
Witoslaw Koczewski <wi@koczewski.de>
2011-07-22 14:43:41 +0200

    iss540 poker action positions

--- src/main/java/scrum/client/estimation/PlanningPokerTableWidget.java
+++ src/main/java/scrum/client/estimation/PlanningPokerTableWidget.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -84,20 +84,10 @@ public class PlanningPokerTableWidget extends AScrumWidget {
 		CloseRequirementEstimationVotingAction close = new CloseRequirementEstimationVotingAction(requirement);
 
 		TableBuilder tb = new TableBuilder();
-		tb.setWidth(null);
-		if (showoff.isExecutable() && showoff.isPermitted()) {
-			tb.add(new HyperlinkWidget(showoff));
-			tb.add(Gwt.createSpacer(10, 1));
-		}
-		if (reset.isExecutable() && reset.isPermitted()) {
-			tb.add(new HyperlinkWidget(reset));
-			tb.add(Gwt.createSpacer(10, 1));
-		}
-		if (close.isExecutable() && close.isPermitted()) {
-			tb.add(new HyperlinkWidget(close));
-			tb.add(Gwt.createSpacer(10, 1));
-		}
-
+		tb.setColumnWidths("33%", "33%", "33%");
+		tb.add(new HyperlinkWidget(showoff));
+		tb.add(new HyperlinkWidget(reset));
+		tb.add(new HyperlinkWidget(close));
 		return tb.createTable();
 	}
 
@@ -180,6 +170,7 @@ public class PlanningPokerTableWidget extends AScrumWidget {
 			this.estimation = estimation;
 		}
 
+		@Override
 		public void onClick(ClickEvent event) {
 			if (estimation >= 0) {
 				requirement.setEstimatedWork(estimation);
@@ -197,6 +188,7 @@ public class PlanningPokerTableWidget extends AScrumWidget {
 			this.estimation = estimation;
 		}
 
+		@Override
 		public void onClick(ClickEvent event) {
 			requirement.setVote(estimation);
 			new VisibleDataChangedEvent().fireInCurrentScope();
@@ -205,6 +197,7 @@ public class PlanningPokerTableWidget extends AScrumWidget {
 
 	private class RemoveEstimationClickHandler implements ClickHandler {
 
+		@Override
 		public void onClick(ClickEvent event) {
 			requirement.setVote(null);
 			new VisibleDataChangedEvent().fireInCurrentScope();
