daf3aec8acd1673d609965c628654310e80dcc78
Witoslaw Koczewski <wi@koczewski.de>
2011-10-17 00:07:52 +0200

    Bugfix

--- src/main/java/scrum/client/collaboration/Emoticon.java
+++ src/main/java/scrum/client/collaboration/Emoticon.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 import scrum.client.admin.Auth;
+import scrum.client.admin.User;
 import scrum.client.common.AScrumGwtEntity;
 
 public class Emoticon extends GEmoticon {
@@ -41,7 +42,9 @@ public class Emoticon extends GEmoticon {
 	}
 
 	public String getTooltip() {
-		return getOwner().getName() + ": " + getEmotionLabel();
+		User owner = getOwner();
+		if (owner == null) return getEmotionLabel();
+		return owner.getName() + ": " + getEmotionLabel();
 	}
 
 	@Override
@@ -61,6 +64,7 @@ public class Emoticon extends GEmoticon {
 
 	public transient static LabelProvider<String> EMOTION_LABEL_PROVIDER = new LabelProvider<String>() {
 
+		@Override
 		public String getLabel(String emotion) {
 			if (emotion == null) return null;
 			return getEmotionLabel(emotion);
