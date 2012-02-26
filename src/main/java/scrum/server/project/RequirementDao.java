fcd5694b106ee1deebe927e256343ced9d62346d
Witoslaw Koczewski <wi@koczewski.de>
2012-02-27 00:07:14 +0100

    iss716 preserve themes when converting issue into a story

--- src/main/java/scrum/server/project/RequirementDao.java
+++ src/main/java/scrum/server/project/RequirementDao.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -29,6 +29,7 @@ public class RequirementDao extends GRequirementDao {
 	public Requirement getRequirementByNumber(final int number, final Project project) {
 		return getEntity(new Predicate<Requirement>() {
 
+			@Override
 			public boolean test(Requirement r) {
 				return r.isNumber(number) && r.isProject(project);
 			}
@@ -53,6 +54,7 @@ public class RequirementDao extends GRequirementDao {
 		requirement.setLabel(issue.getLabel());
 		requirement.setDescription(issue.getDescription());
 		requirement.setIssue(issue);
+		requirement.setThemes(issue.getThemes());
 		issue.setStory(requirement);
 		saveEntity(requirement);
 		requirement.updateNumber();
