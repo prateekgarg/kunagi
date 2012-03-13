3c9d1ee14daf86748801763f55aeea721767ebf2
Witoslaw Koczewski <wi@koczewski.de>
2012-03-14 00:10:48 +0100

    bugfix: comment fields produce new comments every few seconds

--- src/main/java/scrum/client/collaboration/CommentsWidget.java
+++ src/main/java/scrum/client/collaboration/CommentsWidget.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -174,18 +174,7 @@ public class CommentsWidget extends AScrumWidget {
 	}
 
 	private void activateEditor() {
-		this.editor = new RichtextEditorWidget(new ATextEditorModel() {
-
-			@Override
-			public void setValue(String text) {
-				postComment();
-			}
-
-			@Override
-			public String getValue() {
-				return null;
-			}
-		});
+		this.editor = new MyRichtextEditorWidget();
 		this.editor.switchToEditMode();
 		this.editor.setModeSwitchHandler(new AViewEditWidget.ModeSwitchHandler() {
 
@@ -199,6 +188,32 @@ public class CommentsWidget extends AScrumWidget {
 			public void onEditorActivated(AViewEditWidget widget) {}
 		});
 		update();
+	}
+
+	private class MyRichtextEditorWidget extends RichtextEditorWidget {
+
+		public MyRichtextEditorWidget() {
+			super(new ATextEditorModel() {
+
+				@Override
+				public void setValue(String text) {
+					// postComment();
+				}
+
+				@Override
+				public String getValue() {
+					return null;
+				}
+
+			});
+		}
+
+		@Override
+		protected void onEditorClose() {
+			postComment();
+			super.onEditorClose();
+		}
+
 	}
 
 	private class ActivateCommentEditorAction extends AAction {
