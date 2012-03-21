13a161bd5d2b8c9c3aa35aefac9b9e026d46beab
Witoslaw Koczewski <wi@koczewski.de>
2012-03-22 00:11:49 +0100

    sprint projection with 0SP stories

--- src/main/java/scrum/client/project/EstimationBarFactory.java
+++ src/main/java/scrum/client/project/EstimationBarFactory.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -21,43 +21,53 @@ public class EstimationBarFactory {
 
 	public static void createEstimationBars(List<Requirement> requirements, Integer velocity) {
 		int sprintOffset = 0;
-		float remainingWorkInSprint = velocity == null ? 0 : velocity;
-		List<Float> workPerSprint;
-		boolean stop = false;
+		float remainingCapacityInSprint = velocity == null ? 0 : velocity;
+		boolean unestimatedRequirementReached = false;
 		for (Requirement requirement : requirements) {
-			Float estimatedWork = requirement.getEstimatedWork();
-			workPerSprint = new ArrayList<Float>();
+			Float workInRequirement = requirement.getEstimatedWork();
+			List<Float> requirementWorkPerSprint = new ArrayList<Float>();
+
 			if (velocity == null || velocity == 0) {
-				if (estimatedWork != null) workPerSprint.add(estimatedWork);
-				requirement.setEstimationBar(new EstimationBar(0, workPerSprint));
+				// no projection bars -> display as all work done in one sprint
+				if (workInRequirement != null) requirementWorkPerSprint.add(workInRequirement);
+				requirement.setEstimationBar(new EstimationBar(0, requirementWorkPerSprint));
 				continue;
 			}
 
-			if (estimatedWork == null && !stop) {
-				stop = true;
+			if (workInRequirement == null && !unestimatedRequirementReached) {
+				// unestimated Requirement just reached
+				unestimatedRequirementReached = true;
 				sprintOffset++;
 			}
 
-			if (estimatedWork == null) {
-				requirement.setEstimationBar(new EstimationBar(sprintOffset, workPerSprint));
+			if (workInRequirement == null) {
+				// no estimation -> empty bar
+				requirement.setEstimationBar(new EstimationBar(sprintOffset, requirementWorkPerSprint));
 				continue;
 			}
-			float work = estimatedWork;
-			while (work > remainingWorkInSprint) {
-				workPerSprint.add(remainingWorkInSprint);
-				work -= remainingWorkInSprint;
-				if (!stop) sprintOffset++;
-				remainingWorkInSprint = velocity;
+
+			float work = workInRequirement;
+
+			if (work > 0 && remainingCapacityInSprint == 0) {
+				if (!unestimatedRequirementReached) sprintOffset++;
+				remainingCapacityInSprint = velocity;
 			}
-			if (work > 0) {
-				workPerSprint.add(work);
-				remainingWorkInSprint -= work;
+
+			while (work > remainingCapacityInSprint) {
+				// work is more then capacity in sprint -> add indicator to bar
+				requirementWorkPerSprint.add(remainingCapacityInSprint);
+				work -= remainingCapacityInSprint;
+				if (!unestimatedRequirementReached) sprintOffset++;
+				remainingCapacityInSprint = velocity;
 			}
-			requirement.setEstimationBar(new EstimationBar(sprintOffset - workPerSprint.size() + 1, workPerSprint));
-			if (remainingWorkInSprint == 0) {
-				if (!stop) sprintOffset++;
-				remainingWorkInSprint = velocity;
-			}
+
+			// add remaining work to bar
+			requirementWorkPerSprint.add(work);
+			remainingCapacityInSprint -= work;
+
+			requirement.setEstimationBar(new EstimationBar(sprintOffset - requirementWorkPerSprint.size() + 1,
+					requirementWorkPerSprint));
+
 		}
 
 	}
