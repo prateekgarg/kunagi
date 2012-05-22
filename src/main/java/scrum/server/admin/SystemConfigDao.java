eb87c1da663454a1dcf2966cdf3dd1bf2e68ec3a
Witoslaw Koczewski <wi@koczewski.de>
2012-05-23 00:07:34 +0200

    bugfix: systemConfig singleton entity

--- src/main/java/scrum/server/admin/SystemConfigDao.java
+++ src/main/java/scrum/server/admin/SystemConfigDao.java
@@ -1,38 +1,35 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package scrum.server.admin;
 
+import ilarkesto.core.logging.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SystemConfigDao extends GSystemConfigDao {
 
+	private static Log log = Log.get(SystemConfigDao.class);
+
 	public SystemConfig getSystemConfig() {
 		List<SystemConfig> all = new ArrayList<SystemConfig>(getEntities());
 
-		if (all.isEmpty()) {
-			SystemConfig config = newEntityInstance();
-			saveEntity(config);
-			return config;
-		}
+		if (all.isEmpty()) throw new RuntimeException("SystemConfig entity does not exist");
 
-		while (all.size() > 1) {
-			SystemConfig config = all.get(0);
-			deleteEntity(config);
-			all.remove(config);
-		}
+		while (all.size() > 1)
+			throw new RuntimeException("Multiple SystemConfig entities exist");
 
 		return all.get(0);
 	}
@@ -40,6 +37,11 @@ public class SystemConfigDao extends GSystemConfigDao {
 	@Override
 	public void ensureIntegrity() {
 		super.ensureIntegrity();
+		if (getEntities().isEmpty()) {
+			log.warn("SystemConfig entity does not exist. Creating new one.");
+			SystemConfig config = newEntityInstance();
+			saveEntity(config);
+		}
 		getSystemConfig();
 	}
 
