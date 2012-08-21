ede0f3fd308f02c0f2871f5e4b3047bb411f92c0
Witoslaw Koczewski <wi@koczewski.de>
2012-08-22 00:11:41 +0200

    social buttons

--- src/projectHomepage/html/js/kunagihomepage.js
+++ src/projectHomepage/html/js/kunagihomepage.js
@@ -5,26 +5,3 @@ kunagihp.subscribe = function(baseUrl) {
     if (!email) return;
     window.location = baseUrl + email;
 }
-
-kunagihp.sharrre = function() {
-    $('#shareme').sharrre({
-        share: {
-          googlePlus: true,
-          facebook: true,
-          twitter: true,
-          digg: true,
-          delicious: true,
-          stumbleupon: true,
-          linkedin: true
-        },
-        buttons: {
-          googlePlus: {size: 'tall'},
-          facebook: {layout: 'box_count'},
-          twitter: {count: 'vertical'},
-          digg: {type: 'DiggMedium'},
-          delicious: {size: 'tall'},
-          stumbleupon: {layout: '5'},
-          linkedin: {counter: 'top'}
-        }
-      });
-}
