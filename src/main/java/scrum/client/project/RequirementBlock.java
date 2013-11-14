91e7d095bb12823e47375e8ed19285481e909763
stefan-hoehn <stefan.hoehn@aoe.com>
2013-11-14 14:51:18 +0100

    readme etc

--- src/main/java/scrum/client/project/RequirementBlock.java
+++ src/main/java/scrum/client/project/RequirementBlock.java
@@ -52,8 +52,9 @@ public class RequirementBlock extends ABlockWidget<Requirement> implements Trash
 		header.addMenuAction(new RemoveRequirementFromSprintAction(requirement));
 		header.addMenuAction(new MoveRequirementToTopAction(requirement));
 		header.addMenuAction(new MoveRequirementToBottomAction(requirement));
+		header.addMenuAction(new MoveRequirementToOtherProject(requirement));
 		// BlockHeaderWidget dropDownMenu = new BlockHeaderWidget();
-		// dropDownMenu.addMenuAction(new MoveRequirementToOtherProject(requirement));
+		// dropDownMenu.addMenuAction();
 		// header.appendOuterCell(dropDownMenu, null, true);
 		header.addMenuAction(new SetRequirementDirtyAction(requirement));
 		header.addMenuAction(new SetRequirementCleanAction(requirement));
