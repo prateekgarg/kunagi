9a330814b957d62fbc5a38b118825f64de56be76
Witoslaw Koczewski <wi@koczewski.de>
2013-11-14 21:07:41 +0100

    sto221 bullet points on selected text in wiki editor

--- src/main/java/scrum/client/collaboration/Wiki.java
+++ src/main/java/scrum/client/collaboration/Wiki.java
@@ -1,19 +1,20 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
  * 
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package scrum.client.collaboration;
 
+import ilarkesto.core.base.Str;
 import ilarkesto.gwt.client.CodemirrorEditorWidget;
 import ilarkesto.gwt.client.Gwt;
 import ilarkesto.gwt.client.Initializer;
@@ -147,8 +148,7 @@ public class Wiki extends GWiki implements RichtextFormater {
 
 					@Override
 					public void onClick(ClickEvent event) {
-						CodemirrorEditorWidget textArea = editor.getEditor();
-						textArea.wrapSelection("\n# ", "\n# ");
+						bulletize(editor.getEditor(), '#');
 					}
 				}), 0);
 
@@ -157,9 +157,9 @@ public class Wiki extends GWiki implements RichtextFormater {
 
 					@Override
 					public void onClick(ClickEvent event) {
-						CodemirrorEditorWidget textArea = editor.getEditor();
-						textArea.wrapSelection("\n* ", "\n* ");
+						bulletize(editor.getEditor(), '*');
 					}
+
 				}), 0);
 
 			toolbar.insert(createToolbarButton(Img.bundle.h2().createImage(), "Heading 2", new ClickHandler() {
@@ -200,6 +200,23 @@ public class Wiki extends GWiki implements RichtextFormater {
 				}), 0);
 
 		}
+	}
+
+	private void bulletize(CodemirrorEditorWidget textArea, char bulletPoint) {
+		String text = textArea.getSelectedText();
+		if (Str.isBlank(text)) {
+			textArea.wrapSelection("\n\n" + bulletPoint + " ", "");
+			return;
+		}
+		String[] lines = text.split("\n");
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < lines.length; i++) {
+			if (i > 0 || text.startsWith("\n")) sb.append('\n');
+			sb.append(bulletPoint).append(" ").append(lines[i]);
+		}
+		if (text.endsWith("\n")) sb.append("\n");
+		textArea.replaceSelection(sb.toString());
 	}
 
 	protected final Widget createToolbarButton(Image icon, String tooltip, ClickHandler clickHandler) {
