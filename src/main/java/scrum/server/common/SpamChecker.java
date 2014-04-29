6c8dfa20c5ee5dae35fe3c1f122327b5fce03497
Witoslaw Koczewski <wi@koczewski.de>
2014-04-29 08:41:15 +0200

    spam prevention

--- src/main/java/scrum/server/common/SpamChecker.java
+++ src/main/java/scrum/server/common/SpamChecker.java
@@ -1,14 +1,18 @@
 package scrum.server.common;
 
+import ilarkesto.base.Utl;
 import ilarkesto.core.base.Str;
+import ilarkesto.core.logging.Log;
 import ilarkesto.webapp.RequestWrapper;
 import scrum.server.WebSession;
 
 public class SpamChecker {
 
-	public static void check(String text, String name, String email, RequestWrapper<WebSession> req) {
+	private static final Log log = Log.get(SpamChecker.class);
+
+	private static int score(String text, String name, String email, RequestWrapper<WebSession> req) {
 		String spamPreventionCode = req.get("spamPreventionCode");
-		if (!"no-spam".equals(spamPreventionCode)) throw new RuntimeException("HTTP Request identified as SPAM.");
+		if (!"no-spam".equals(spamPreventionCode)) return 100;
 
 		int spamScore = 0;
 
@@ -24,12 +28,21 @@ public class SpamChecker {
 		if (text != null) {
 			text = text.toLowerCase();
 			if (text.contains("http://") || text.contains("https://")) spamScore += 25;
-			if (text.contains("Very nice site!")) spamScore += 25;
+			if (text.contains("very nice site!")) spamScore += 25;
 			if (text.contains("cheap goods")) spamScore += 25;
 			if (text.contains("opeaixy2.com")) spamScore += 49;
 		}
 
-		if (spamScore >= 50) throw new RuntimeException("HTTP Request identified as SPAM.");
+		return spamScore;
+	}
+
+	public static void check(String text, String name, String email, RequestWrapper<WebSession> req) {
+		int score = score(text, name, email, req);
+		if (score >= 50) {
+			log.info("SPAM identified:", name, email, text);
+			Utl.sleep(10000);
+			throw new RuntimeException("HTTP Request identified as SPAM.");
+		}
 	}
 
 }
