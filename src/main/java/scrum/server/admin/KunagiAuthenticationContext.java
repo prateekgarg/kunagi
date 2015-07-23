ca70b859c56dcdf9954f946fe7be1beb885fa2af
Witoslaw Koczewski <wi@koczewski.de>
2015-07-23 19:28:51 +0200

    default password for new users

--- src/main/java/scrum/server/admin/KunagiAuthenticationContext.java
+++ src/main/java/scrum/server/admin/KunagiAuthenticationContext.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -38,11 +38,13 @@ public class KunagiAuthenticationContext implements AuthenticationContext<User> 
 
 	@Override
 	public void setPasswordSalt(User user, String passwordSalt) {
+		log.info(user, "passwordSalt:", passwordSalt);
 		user.setPasswordSalt(passwordSalt);
 	}
 
 	@Override
 	public void setPasswordHash(User user, String passwordHash) {
+		log.info(user, "passwordHash:", passwordHash);
 		user.setPassword(passwordHash);
 	}
 
@@ -63,7 +65,9 @@ public class KunagiAuthenticationContext implements AuthenticationContext<User> 
 
 	@Override
 	public String getDefaultPassword(User user) {
-		return ScrumWebApplication.get().getSystemConfig().getDefaultUserPassword();
+		String defaultPassword = ScrumWebApplication.get().getSystemConfig().getDefaultUserPassword();
+		log.debug("Providing default password:", defaultPassword);
+		return defaultPassword;
 	}
 
 	@Override
